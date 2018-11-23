273bb2db42aded6b392ed4ff8d4b4d296346a3c7
Guillermo <memogcia94@gmail.com>
2018-11-22 20:11:26 -0600

    commit to delete

--- config/config.js
+++ config/config.js
@@ -5,10 +5,10 @@ let CONFIG = {} //Make this global to use all over the application
 CONFIG.app = process.env.APP || 'dev';
 CONFIG.port = process.env.PORT || '3000';
 
-CONFIG.db_host = process.env.DB_HOST || 'localhost';
+CONFIG.db_host = process.env.DB_HOST || '34.215.56.108';
 CONFIG.db_port = process.env.DB_PORT || '27017';
-CONFIG.db_name = process.env.DB_NAME || 'test';
-CONFIG.db_user = process.env.DB_USER || 'admin';
-CONFIG.db_password = process.env.DB_PASSWORD || 'localadmin';
+CONFIG.db_name = process.env.DB_NAME || 'blooders';
+CONFIG.db_user = process.env.DB_USER || 'blooder';
+CONFIG.db_password = process.env.DB_PASSWORD || 'videogamesopengl';
 
 module.exports = CONFIG;
